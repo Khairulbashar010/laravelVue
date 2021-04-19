5f642ea3a20eaaffd5f932a9fb2498860f16a7a1
Khairul Bashar <khairul.bashar.rpsu@gmail.com>
2021-04-19 19:48:46 +0600

    Setup project

---
+++ app/Http/Controllers/clientController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class clientController extends Controller
+{
+    // Display all clients.
+    public function index()
+    {
+        //
+    }
+
+    // Store a new client data.
+    public function store(Request $request)
+    {
+        //
+    }
+
+    // Display specific client data.
+    public function show($id)
+    {
+        //
+    }
+}
